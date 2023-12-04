c9b1e68c0a6dc563e44743f40456e5d6814bf9ab
PranshulGG <134786144+PranshulGG@users.noreply.github.com>
2023-12-04 21:52:39 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function showScrollbar() {
+    document.querySelector('.side-nav').classList.add('show-scrollbar');
+    document.querySelector('.vid-container').classList.add('show-scrollbar');
+}
+
+function hideScrollbar() {
+    document.querySelector('.side-nav').classList.remove('show-scrollbar');
+    document.querySelector('.vid-container').classList.remove('show-scrollbar');
+}
